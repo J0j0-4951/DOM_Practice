6fe29b146dc8803f667955616ae2d1c5d349a882
J0j0-4951 <jhos3lin3s.z@gmail.com>
2025-06-17 20:40:42 +0000

    Add command

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-documnet.gitElementById("greeting").textContent("Hi, there!");
+document.gitElementById("greeting").textContent = "Hi, there!";
+document.gitElementById("h1").textContent = "The Header";
+
+/*"The Header"*/
 
 /*
 run this first:php -S localhost:3006 -t ./
