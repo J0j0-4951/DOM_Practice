b7591a9df9600c82b93d0c138dafe58fc41e97f6
J0j0-4951 <jhos3lin3s.z@gmail.com>
2025-06-17 21:09:25 +0000

    text change

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 document.getElementById("greeting").textContent = "Hi, there!";
 document.getElementById("h1").textContent = "The Header";
+let items = document.getElementByClassName("redclass");
+for (let i=0; i< items.legth; i++)
+{
+    items[i].textContent = "Text.change";
+}
 
 /*"The Header"*/
 
